90c12e7b198f77c943a1e7164c0a93e82d422f9a
Jonathan Clem <jotclem@gmail.com>
2016-06-27 15:48:24 -0400

    Add Slack.IM methods

---
+++ lib/slack/im.ex
@@ -0,0 +1,74 @@
+defmodule Slack.IM do
+  @moduledoc """
+  Functions for direct messages
+  """
+
+  @base "im"
+
+  use Slack.Request
+
+  @doc """
+  Close an IM channel
+
+  https://api.slack.com/methods/im.close
+
+  ## Examples
+
+      Slack.client(token)
+      |> Slack.IM.close(channel: "D1234567890")
+  """
+  @spec close(Slack.Client.t, Keyword.t) :: Slack.response
+  defpost :close
+
+  @doc """
+  Get the history of an IM channel.
+
+  https://api.slack.com/methods/im.history
+
+  ## Examples
+
+      Slack.client(token)
+      |> Slack.IM.history(channel: "D1234567890")
+  """
+  @spec history(Slack.Client.t, Keyword.t) :: Slack.response
+  defget :history
+
+  @doc """
+  List IM channels for the user.
+
+  https://api.slack.com/methods/im.list
+
+  ## Examples
+
+      Slack.client(token)
+      |> Slack.IM.list
+  """
+  @spec list(Slack.Client.t, Keyword.t) :: Slack.response
+  defget :list
+
+  @doc """
+  Move the read cursor in an IM channel.
+
+  https://api.slack.com/methods/im.mark
+
+  ## Examples
+
+      Slack.client(token)
+      |> Slack.IM.mark(channel: "D1234567890", ts: 1234567890.123456)
+  """
+  @spec mark(Slack.Client.t, Keyword.t) :: Slack.response
+  defpost :mark
+
+  @doc """
+  Open an IM channel with a user.
+
+  https://api.slack.com/methods/im.open
+
+  ## Examples
+
+      Slack.client(token)
+      |> Slack.IM.open(user: "U1234567890")
+  """
+  @spec open(Slack.Client.t, Keyword.t) :: Slack.response
+  defpost :open
+end
