79b23f2f96bb6fa27f5ff54392da3116fb64b09e
Jonathan Clem <jotclem@gmail.com>
2016-06-27 11:18:49 -0400

    Add Slack.Auth methods

---
+++ lib/slack/auth.ex
@@ -0,0 +1,41 @@
+defmodule Slack.Auth do
+  @moduledoc """
+  Functions for dealing with access tokens
+  """
+
+  @base "auth"
+
+  use Slack.Request
+
+  @doc """
+  Revoke an access token.
+
+  https://api.slack.com/methods/auth.revoke
+
+  ## Examples
+
+      Slack.client(token)
+      |> Slack.Auth.revoke
+  """
+  @spec revoke(Slack.Client.t, Keyword.t) :: Slack.response
+  defrequest revoke(client, body \\ []) do
+    body = body |> Keyword.put(:token, client.token)
+    Slack.post "#{@base}.revoke", {:form, body}
+  end
+
+  @doc """
+  Check and identity an access token.
+
+  https://api.slack.com/methods/auth.test
+
+  ## Examples
+
+      Slack.client(token)
+      |> Slack.Auth.test
+  """
+  @spec test(Slack.Client.t, Keyword.t) :: Slack.response
+  defrequest test(client, body \\ []) do
+    body = body |> Keyword.put(:token, client.token)
+    Slack.post "#{@base}.test", {:form, body}
+  end
+end
