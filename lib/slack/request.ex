085967f3617ff5eca7c6e7f5b4205331eab17460
Jonathan Clem <jotclem@gmail.com>
2016-06-27 16:28:14 -0400

    Document macros

--- lib/slack/request.ex
+++ lib/slack/request.ex
@@ -5,6 +5,10 @@ defmodule Slack.Request do
     end
   end
 
+  @doc """
+  Define a method `method` that makes a GET request to the module's base
+  endpoint plus the method name: e.g. `baseEndpoint.methodName`.
+  """
   defmacro defget(method) do
     quote do
       defrequest unquote(method)(client, query \\ []) do
@@ -14,6 +18,10 @@ defmodule Slack.Request do
     end
   end
 
+  @doc """
+  Define a method `method` that makes a POST request to the module's base
+  endpoint plus the method name: e.g. `baseEndpoint.methodName`.
+  """
   defmacro defpost(method) do
     quote do
       defrequest unquote(method)(client, body \\ []) do
@@ -25,6 +33,17 @@ defmodule Slack.Request do
     end
   end
 
+  @doc """
+  Define a method `method` that makes a request (the block must return a call
+  to an HTTPoison request method).
+
+  The return of the request will be pattern matched against Slack's standard
+  `%{"ok" => true}` message format, and will return `{:ok, response}` when
+  the body matches `%{"ok" => true}`, or `{:error, response}` otherwise.
+
+  In the case of an actual failed request, `{:error, HTTPoison.Error.t}` will be
+  returned.
+  """
   defmacro defrequest(signature, do: body) do
     quote do
       def unquote(signature) do
