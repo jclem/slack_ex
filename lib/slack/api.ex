7ef669d0560c748392b54aa6a049caf6a27f47fb
Jonathan Clem <jotclem@gmail.com>
2016-06-27 11:12:44 -0400

    Add Slack.API methods

---
+++ lib/slack/api.ex
@@ -0,0 +1,22 @@
+defmodule Slack.API do
+  @moduledoc """
+  Functions for checking calling code
+  """
+
+  @base "api"
+
+  use Slack.Request
+
+  @doc """
+  Test calling code.
+
+  https://api.slack.com/methods/api.test
+
+  ## Examples
+
+      Slack.client(token)
+      |> Slack.API.test(foo: "bar")
+  """
+  @spec test(Slack.Client.t, Keyword.t) :: Slack.response
+  defpost :test
+end
